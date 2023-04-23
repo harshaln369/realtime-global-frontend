9a5c6d3c578723e5e20e42b1eda6501a3d9da07e
Harshal Nikhare <harshaln369@gmail.com>
2023-04-23 18:01:33 +0530

    infinite scrolling done

--- src/routes/notes.jsx
+++ src/routes/notes.jsx
@@ -2,38 +2,46 @@ import React, { useState, useEffect } from "react";
 import { useParams, useLoaderData } from "react-router-dom";
 import io from "socket.io-client";
 import axios from "axios";
-// import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from "react-infinite-scroll-component";
 import MultipleSelectChip from "../components/selectChip";
 import BasicSelect from "../components/basicSelect";
 import { BASE_URL } from "../constants";
 const socket = io.connect(BASE_URL);
 
 export async function loader(filterData) {
-  const { data } = await axios.post(`${BASE_URL}/notes`, filterData);
+  const { data } = await axios.post(`${BASE_URL}/notes`, {
+    ...filterData,
+    limit: 10,
+  });
   return {
     fetchedNotes: data.data.notes,
     priorities: data.data.priorities,
     users: data.data.users,
+    count: data.data.count,
   };
 }
 
 const fetchNotes = async (filterData) => {
-  const { data } = await axios.post(`${BASE_URL}/notes`, filterData);
+  const { data } = await axios.post(`${BASE_URL}/notes`, {
+    ...filterData,
+    limit: 10,
+  });
   return data;
 };
 
-// let skip = 0;
-// const fetchMoreData = async (setNotes, notes, setHasMore, count) => {
-//   if (notes >= count) setHasMore(false);
-//   const { data } = await axios.get(`${BASE_URL}/notes/${skip}`);
-//   console.log("more fetched data", data.data.notes);
-//   setNotes([...notes, ...data.data.notes]);
-//
-//   skip += 10;
-// };
+const fetchMoreData = async (notes, setNotes, setHasMore, count) => {
+  if (notes.length >= count) {
+    setHasMore(false);
+    return;
+  }
+  const limit = parseInt(notes.length) * 10 + 10;
+
+  const { data } = await axios.post(`${BASE_URL}/notes`, { limit });
+  setNotes(data.data.notes);
+};
 
 const Notes = () => {
-  const { fetchedNotes, priorities, users } = useLoaderData();
+  const { fetchedNotes, priorities, users, count } = useLoaderData();
   console.log("api data", fetchedNotes, priorities, users);
   const [note, setNote] = useState("");
   const [priority, setPriority] = useState("");
@@ -47,7 +55,7 @@ const Notes = () => {
   const [selectedPriorities, setSelectedPriorities] = React.useState([]);
 
   const [sort, setSort] = React.useState("");
-  // const [hasMore, setHasMore] = useState(count > 10 ? true : false);
+  const [hasMore, setHasMore] = useState(count > 10 ? true : false);
   const { userId } = useParams();
 
   const handleEditSave = async (note) => {
@@ -180,70 +188,70 @@ const Notes = () => {
           setSelect={setSort}
         />
         <ul>
-          {/* <InfiniteScroll */}
-          {/*   dataLength={notes.length} //This is important field to render the next data */}
-          {/*   next={() => fetchMoreData(setNotes, notes, setHasMore, count)} */}
-          {/*   hasMore={hasMore} */}
-          {/*   loader={<h4>Loading...</h4>} */}
-          {/*   endMessage={ */}
-          {/*     <p style={{ textAlign: "center" }}> */}
-          {/*       <b>Yay! You have seen it all</b> */}
-          {/*     </p> */}
-          {/*   } */}
-          {/* > */}
-          {notes?.length ? (
-            notes.map((note) => (
-              <li key={note.createdAt}>
-                {isEdit.value && isEdit._id === note._id ? (
-                  <>
-                    <input
-                      type="text"
-                      value={editVal}
-                      onChange={(e) => setEditVal(e.target.value)}
-                    />
+          <InfiniteScroll
+            dataLength={notes.length} //This is important field to render the next data
+            next={() => fetchMoreData(notes, setNotes, setHasMore, count)}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            {notes?.length ? (
+              notes.map((note) => (
+                <li key={note.createdAt}>
+                  {isEdit.value && isEdit._id === note._id ? (
+                    <>
+                      <input
+                        type="text"
+                        value={editVal}
+                        onChange={(e) => setEditVal(e.target.value)}
+                      />
 
-                    <BasicSelect
-                      items={["High", "Medium", "Low"]}
-                      name="Priority"
-                      select={editPriority}
-                      setSelect={setEditPriority}
-                    />
-                    <button onClick={() => handleEditSave(note)}>Save</button>
-                  </>
-                ) : (
-                  <>
-                    <h4>{note.note}</h4>
-                    <h6>{note.priority}</h6>
-                    <div>{`Contributed By: ${[
-                      ...new Set(note.contributedBy),
-                    ].join(", ")}`}</div>
-                    <h6>History</h6>
-                    <ul>
-                      {note.history.map((el) => (
-                        <li
-                          key={el.id}
-                        >{`Note: ${el.note}, ContributedBy: ${el.contributedBy}`}</li>
-                      ))}
-                    </ul>
-                    <button
-                      onClick={() => {
-                        setIsEdit({ value: true, _id: note._id });
-                        setEditVal(note.note);
-                      }}
-                    >
-                      Edit
-                    </button>
-                    <button onClick={() => handleDelete({ _id: note._id })}>
-                      Delete
-                    </button>
-                  </>
-                )}
-              </li>
-            ))
-          ) : (
-            <h4>No Notes Found</h4>
-          )}
-          {/* </InfiniteScroll> */}
+                      <BasicSelect
+                        items={["High", "Medium", "Low"]}
+                        name="Priority"
+                        select={editPriority}
+                        setSelect={setEditPriority}
+                      />
+                      <button onClick={() => handleEditSave(note)}>Save</button>
+                    </>
+                  ) : (
+                    <>
+                      <h4>{note.note}</h4>
+                      <h6>{note.priority}</h6>
+                      <div>{`Contributed By: ${[
+                        ...new Set(note.contributedBy),
+                      ].join(", ")}`}</div>
+                      <h6>History</h6>
+                      <ul>
+                        {note.history.map((el) => (
+                          <li
+                            key={el.id}
+                          >{`Note: ${el.note}, ContributedBy: ${el.contributedBy}`}</li>
+                        ))}
+                      </ul>
+                      <button
+                        onClick={() => {
+                          setIsEdit({ value: true, _id: note._id });
+                          setEditVal(note.note);
+                        }}
+                      >
+                        Edit
+                      </button>
+                      <button onClick={() => handleDelete({ _id: note._id })}>
+                        Delete
+                      </button>
+                    </>
+                  )}
+                </li>
+              ))
+            ) : (
+              <h4>No Notes Found</h4>
+            )}
+          </InfiniteScroll>
         </ul>
       </div>
     </>
